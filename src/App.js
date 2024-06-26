3af1f4dd1dabf06e48aaaf4163f510f103611175
Sameeksha224 <sameekshaprusty@gmail.com>
2024-06-26 12:37:58 +0530

    Added new files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import AddProjectSimple from './AddProjectSimple';
+import BasicSelect from "./select-options";
+import AxiosUse from './AxiosUse';
+
+function App() {
+  return (
+    <>
+    <AddProjectSimple />
+  {/* <BasicSelect/> */}
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/AxiosUse.jsx
@@ -0,0 +1,18 @@
+// import axios from 'axios';
+// import React, { useEffect } from 'react'
+
+// function AxiosUse(){
+//     const [userData , setUseData] = useState([])
+//     useEffect(()=>{
+//         axios.get("http://127.0.0.1:8000/api/getSurveyType")
+//         .then((response)=>{
+//             console.log(response);
+//             setUseData(response.data)
+//         })
+//     },[])
+//     return(
+//         <div></div>
+//     )
+// }
+
+// export default AxiosUse;
